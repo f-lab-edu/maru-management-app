76d31376c1a19a54f1c728e6285036bdbdf09ead
Cupcakes33 <rute930627@gmail.com>
2025-11-19 16:38:21 +0900

    refactor: OAuthUrlRes를 record로 변경

--- backend/src/main/java/com/maru/controller/auth/dto/OAuthUrlRes.java
+++ backend/src/main/java/com/maru/controller/auth/dto/OAuthUrlRes.java
@@ -1,12 +1,5 @@
 package com.maru.controller.auth.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-@Builder
-public class OAuthUrlRes {
-    private String authorizationUrl;
-}
+public record OAuthUrlRes(
+        String authorizionUrl
+) {}
