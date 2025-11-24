87d6603e3ae3d6fe734a83dc34af4601bde1431c
Cupcakes33 <rute930627@gmail.com>
2025-11-24 17:54:29 +0900

    fix : google oauth 서버 오류 수정
    - application-*.yml 에 cors:max-age 추가
    - GoogleUserInfoRes 의 id에 JsonAlias 추가

--- backend/src/main/java/com/maru/service/auth/dto/GoogleUserInfoRes.java
+++ backend/src/main/java/com/maru/service/auth/dto/GoogleUserInfoRes.java
@@ -1,6 +1,9 @@
 package com.maru.service.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 public record GoogleUserInfoRes(
+    @JsonAlias({"sub", "id"})
     String id,
     String email,
     String name,
