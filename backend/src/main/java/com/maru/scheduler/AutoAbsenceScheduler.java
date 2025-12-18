5c569facb97d03374b749e1acb2b45667993e1e4
Cupcakes33 <rute930627@gmail.com>
2025-12-18 22:07:21 +0900

    refactor : 자동 결석 범위를 주중으로 변경
    - cron 식을 월-금 09시로 수정

--- backend/src/main/java/com/maru/scheduler/AutoAbsenceScheduler.java
+++ backend/src/main/java/com/maru/scheduler/AutoAbsenceScheduler.java
@@ -17,9 +17,9 @@ public class AutoAbsenceScheduler {
 
     /**
      * 자동 결석 처리 스케줄러
-     * 매일 09:00에 전날 출석 기록이 없는 활성 원생을 결석 처리
+     * 주중 09:00에 전날 출석 기록이 없는 활성 원생을 결석 처리
      */
-    @Scheduled(cron = "0 0 9 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 0 9 * * MON-FRI", zone = "Asia/Seoul")
     public void processAutoAbsence() {
         LocalDate yesterday = LocalDate.now().minusDays(1);
         log.info("자동 결석 처리 시작: date={}", yesterday);
