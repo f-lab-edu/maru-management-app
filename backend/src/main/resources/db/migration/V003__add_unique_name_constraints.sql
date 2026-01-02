03c69fe1b8b4c5a358bd2689df9b8171cb01f1a4
Cupcakes33 <rute930627@gmail.com>
2026-01-02 11:21:06 +0900

    refactor: 수련부 이름 중복 제약조건 마이그레이션 삭제
    - V003__add_unique_name_constraints.sql 파일 삭제

--- backend/src/main/resources/db/migration/V003__add_unique_name_constraints.sql
+++
@@ -1,7 +0,0 @@
--- Section, Classes 테이블에 이름 중복 방지 UNIQUE 제약 추가
-
-ALTER TABLE section
-    ADD CONSTRAINT uk_section_dojang_name UNIQUE (dojang_id, name);
-
-ALTER TABLE classes
-    ADD CONSTRAINT uk_classes_section_name UNIQUE (section_id, name);
